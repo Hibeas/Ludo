6fb08056e5e2142cc88058facab077eb3661e559
Hibeas <szymon.skarbek@gmail.com>
2026-03-23 22:58:22 +0100

    Movent of pawn is working with dice rolling - no selecting the pawn

--- game_logic.py
+++ game_logic.py
@@ -1,13 +1,14 @@
 import pygame
 import random
 import time
+import constants
 
 
 class Pawn:
     def __init__(self, color, pawn_id, start_pos_xy):
         self.color = color      
         self.pawn_id = pawn_id  
-        self.position = 0       # 0 = Yard, 1-40 = Board, 99 = Home
+        self.position = 0       # 0 = Yard, 1-57 = Board, 99 = Home
         self.is_home = False
         self.board_index = 0
         self.move_timer = 0
@@ -29,13 +30,48 @@ class Pawn:
     def draw(self, surface):
         surface.blit(self.image, self.rect)
         
-    def auto_walk(self, board_list):
-        if self.board_index < len(board_list):
-            self.move_timer += 1
-            if self.move_timer >= self.move_speed:
-                self.rect.center = board_list[self.board_index]
-                self.board_index += 1
-                self.move_timer = 0 # Reset timer
+        
+        
+        
+    def update_screen_pos(self, board_index):
+        if self.position < 53:
+            self.screen_pos = constants.BOARD[board_index]
+            self.rect.center = self.screen_pos
+        else:
+            if self.color == "blue":
+                self.screen_pos = constants.BLUE_HOME[board_index-52] 
+                self.rect.center = self.screen_pos
+                self.is_home = True
+            elif self.color == "green":
+                self.screen_pos = constants.GREEN_HOME[board_index-52]
+                self.rect.center = self.screen_pos
+                self.is_home = True
+                
+    def move(self, dice):
+        if self.position == 0 and dice == 6:
+            self.position = 1
+            if(self.color == "blue"):
+                self.board_index = 0
+                self.update_screen_pos(0)
+            elif (self.color == "green"):
+                self.board_index = 26
+                self.update_screen_pos(26)
+        elif self.position > 0 and not self.is_home:
+            self.board_index = self.board_index + dice
+            self.position = self.position + dice
+            self.update_screen_pos(self.board_index)
+        elif self.is_home:
+            
+            current_home_idx = self.board_index - 52
+            
+            if current_home_idx + dice < 6:
+                self.board_index += dice
+                self.position += dice
+                self.update_screen_pos(self.board_index)
+                if self.board_index - 52 == 5:
+                    print(f"{self.color} pawn {self.pawn_id} has FINISHED!")
+            else:
+                print("Roll too high to move further in home lane!")
 
 
 
@@ -44,12 +80,13 @@ class Dice:
     def __init__(self, position):
         self.position = position
         self.images = [pygame.image.load(f"assets/dice_{i}.png").convert_alpha() for i in range(1, 7)]
-        self.current_value = 1
+        self.current_value = 0
         self.is_rolling = False
         self.roll_start_time = 0
         self.roll_duration = 1.0  
         self.last_frame_time = 0
         self.frame_delay = 0.1
+        self.new_value = False
 
     def start_roll(self):
         if not self.is_rolling:
@@ -68,8 +105,10 @@ class Dice:
                     self.last_frame_time = now 
             else:
                 self.is_rolling = False
+                self.new_value = True
                 self.current_value = random.randint(1, 6)
                 print("Dice rolled:", self.current_value)
+                return self.current_value
 
     def draw(self, surface):
         img = self.images[self.current_value - 1]
